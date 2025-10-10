11f09cbc505a6629f576d1066ae95a25e024fe14
Abhijeet Singh Parihar <abhibuisness123@gmail.com>
2025-10-10 21:33:25 +0530

    updated resume

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -41,7 +41,7 @@ export const AboutMe = () => {
                         Get In Touch
                     </a>
 
-                    <a href="/Resume/Abhijeet Singh Parihar Resume.pdf" download className="px-6 py-2 rounded-full  border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
+                    <a href="/Resume/Abhijeet Singh Parihar_Resume.pdf" download className="px-6 py-2 rounded-full  border border-primary text-primary hover:bg-primary/10 transition-colors duration-300">
                         Download CV
                     </a>
                 </div>
